0913efd940623e4918625b71c2bddf95bbabc4f5
kyungae-Han <kyung03313@gmail.com>
2025-09-19 18:06:31 +0900

    개인정보취급방침 약관작성중

---
+++ src/main/java/com/dev/BionLifeScienceWeb/controller/CompanyController.java
@@ -0,0 +1,23 @@
+package com.dev.BionLifeScienceWeb.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/front/company")
+public class CompanyController {
+
+    @GetMapping("/privacy-policy")
+    public String privacyPolicy() {
+        return "front/company/privacy-policy";
+    }
+
+    @GetMapping("/privacy-policy/iframe")
+    public String privacyPolicyIframe(HttpServletResponse res) {
+        res.setHeader("X-Frame-Options", "SAMEORIGIN");
+        res.setHeader("Content-Security-Policy", "frame-ancestors 'self'");
+        return "front/company/privacy-policy";
+    }
+}
