37322f70e5ad0c8f8b0211b2f102c29cf2a301cf
kyungae-Han <kyung03313@gmail.com>
2025-11-03 17:49:50 +0900

    이메일 수정중

--- src/main/java/com/dev/BionLifeScienceWeb/service/EmailSendService.java
+++ src/main/java/com/dev/BionLifeScienceWeb/service/EmailSendService.java
@@ -1,7 +1,5 @@
 package com.dev.BionLifeScienceWeb.service;
 
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -26,15 +24,15 @@ public class EmailSendService implements EmailSendable{
              MimeMessage mimeMessage = javaMailSender.createMimeMessage();
              MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
-             helper.setTo(to);                                
-             helper.setFrom("admin@bionlifescience.com", "Bion Life Science");  
+             helper.setTo(to);                                 // 수신자
+             helper.setFrom("info@bionlifescience.com");       // SMTP 인증 계정
              helper.setReplyTo(replyTo); 
-             helper.setSubject(subject);                       
-             helper.setText(message, true);                   
+             helper.setSubject(subject);                       // 제목
+             helper.setText(message, true);                    // 본문 (true = HTML 지원)
 
              javaMailSender.send(mimeMessage);
 
-         } catch (MessagingException | UnsupportedEncodingException e) {
+         } catch (MessagingException e) {
              throw new RuntimeException("메일 전송 실패", e);
          }
      }
