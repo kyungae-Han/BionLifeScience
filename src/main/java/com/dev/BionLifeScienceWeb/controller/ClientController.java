93265bede757dd65b630c7ae2adddf1c1284a1ec
kyungae-Han <kyung03313@gmail.com>
2025-12-12 11:01:19 +0900

    수신메일 확장자 검증 로직 추가

--- src/main/java/com/dev/BionLifeScienceWeb/controller/ClientController.java
+++ src/main/java/com/dev/BionLifeScienceWeb/controller/ClientController.java
@@ -38,6 +38,52 @@ public class ClientController {
 	        MultipartFile file,
 	        HttpServletResponse response
 	) throws IOException {
+		
+		
+		final List<String> BLOCK_EXT = List.of("php", "exe", "scr", "bat", "cmd", "js", "jse", "vbs", "vbe", "jar");
+		
+		final List<String> MACRO_EXT = List.of("docm", "xlsm", "pptm");
+		
+		final List<String> ALLOW_EXT =  List.of("doc", "docx", "xls", "xlsx", "ppt", "pptx", "pdf", "jpg", "jpeg", "png", "gif", "txt", "zip", "rar", "7z");
+		
+		String filename = null;
+		String ext = null;
+		
+		if(file != null && !file.isEmpty()) {
+			
+			filename = file.getOriginalFilename().toLowerCase();
+			
+			if(filename.contains(".")) {
+				ext = filename.substring(filename.lastIndexOf(".")+1).toLowerCase();
+			}
+			
+		
+			if (BLOCK_EXT.contains(ext) || MACRO_EXT.contains(ext)) {
+				
+				response.setContentType("test/html; charset=UTF-8");
+				response.getWriter().write("""
+		                <script>
+	                    alert('보안상 해당 확장자 파일은 업로드할 수 없습니다.');
+	                    history.back();
+	                </script>
+	            """);
+				return;
+			}
+			
+			
+			if(!ALLOW_EXT.contains(ext)) {
+				response.setContentType("text/html; charset=UTF-8");
+				response.getWriter().write("""
+		                <script>
+	                    alert('지원되지 않는 파일 형식입니다. PDF, DOCX, XLSX, ZIP 등 정상 문서만 업로드 가능합니다.');
+	                    history.back();
+	                </script>
+	            """);
+				return;
+			}
+			
+		}
+		
 	    clientService.clientInsert(client, file);
 	    List<CompanyEmail> list = companyEmailRepository.findAll();
 	
@@ -59,6 +105,8 @@ public class ClientController {
 	            window.location.href = '/index';
 	        </script>
 	    """);
+	    
+	    
 	}
 	
     @PostMapping("/clientDelete")
