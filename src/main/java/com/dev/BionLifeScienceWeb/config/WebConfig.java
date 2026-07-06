014dbcc941fd6aea13c65e6e9c3f959b106d28ed
kyungae-Han <kyung03313@gmail.com>
2026-07-06 14:47:15 +0900

    보안 유효성 검사 및 메인화면 로딩 지연 수정

--- src/main/java/com/dev/BionLifeScienceWeb/config/WebConfig.java
+++ src/main/java/com/dev/BionLifeScienceWeb/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.dev.BionLifeScienceWeb.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/upload/**")
-            .addResourceLocations("file:" + uploadPath + "/");
+            .addResourceLocations("file:" + uploadPath + "/")
+            .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS).cachePublic());
   }
 
   @Override
